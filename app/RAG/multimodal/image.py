3be92f0a65840ec387a3e25c45980060bfef2cb3
Khaled Abo <36343726+Khaled85abo@users.noreply.github.com>
2024-10-22 23:34:44 +0200

    Make exit button for modal bigger

--- app/RAG/multimodal/image.py
+++ app/RAG/multimodal/image.py
@@ -1,5 +1,6 @@
 from llms import chain_gpt_4_vision
 from langchain.schema.messages import HumanMessage, AIMessage
+import base64
 
 # Function for image summaries
 def summarize_image(encoded_image):
@@ -17,3 +18,37 @@ def summarize_image(encoded_image):
     ]
     response = chain_gpt_4_vision.invoke(prompt)
     return response.content
+
+
+def encode_image(image_path):
+  with open(image_path, "rb") as image_file:
+    return base64.b64encode(image_file.read()).decode('utf-8')
+
+# Path to your image
+image_path = "img1"
+
+# Getting the base64 string
+base64_image = encode_image(image_path)
+
+response = client.chat.completions.create(
+  model="gpt-4o-mini",
+  messages=[
+    {
+      "role": "user",
+      "content": [
+        {
+          "type": "text",
+          "text": "You are a helpful ai assistant, describe the image and provide a detailed explanation of it, return the explanation in an array of strings to be used for vectorization, make sure to include all the details of the image and provide an decent OVERLAP of the description! NO introductino needed.",
+        },
+        {
+          "type": "image_url",
+          "image_url": {
+            "url":  f"data:image/jpeg;base64,{base64_image}"
+          },
+        },
+      ],
+    }
+  ],
+)
+
+print(response.choices[0])
